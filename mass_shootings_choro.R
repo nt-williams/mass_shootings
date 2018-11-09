b2f2bd69707114c37342119906b677f5a1d4f61f
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-11-08 19:58:45 -0500

    legend edits

--- mass_shootings_choro.R
+++ mass_shootings_choro.R
@@ -36,18 +36,18 @@ mass_location %>%
   ggplot() + 
   geom_polygon(aes(x = long, y = lat, fill = n, group = group), color = NA, alpha = 0.9) + 
   labs(title = "Number of mass shootings in U.S. states", 
-       caption = "Data: Stanford Mass Shootings in America, courtesy of the Stanford Geospatial Center and Stanford Libraries") +
+       caption = "Data source: Stanford Mass Shootings in America, courtesy of the Stanford Geospatial Center and Stanford Libraries") +
   scale_fill_viridis(breaks = c(0, 5, 10, 15, 20, 25, 30, 35, 40), 
                      name = "Number of mass shootings", 
-                     guide = guide_colorbar(barheight = unit(2, units = "mm"),
-                                            barwidth = unit(50, units = "mm"),
+                     guide = guide_colorbar(barheight = unit(3, units = "mm"),
+                                            barwidth = unit(70, units = "mm"),
                                             title.position = 'top', nrow = 1)) +
   theme_void() + 
   coord_map() + 
   theme(plot.background = element_rect(fill = "#f5f5f2", color = NA), 
         legend.position = c(0.15, 0.09), 
         legend.direction = "horizontal", 
-        plot.caption = element_text(size = 8, color = "#4e4d47", margin = margin(b = 0.3, unit = "cm"), hjust = 0.9), 
+        plot.caption = element_text(size = 8, color = "#4e4d47", margin = margin(b = 0.3, unit = "cm"), hjust = 0.98), 
         legend.title = element_text(size = 10), 
         plot.title = element_text(size = 12, hjust = 0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")))
 
