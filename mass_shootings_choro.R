877a94e0490668fbed422631737b5182d650293e
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-11-08 19:05:25 -0500

    cleaned mass shooting data

--- mass_shootings_choro.R
+++ mass_shootings_choro.R
@@ -1,16 +1,32 @@
 
 library(tidyverse)
+library(lubridate)
 
-# pulling data
+# pulling and manipulating mass shooting data
 
 mass_shooting <- 
-  read_csv("https://raw.githubusercontent.com/StanfordGeospatialCenter/MSA/master/Data/Stanford_MSA_Database.csv")
+  read_csv("https://raw.githubusercontent.com/StanfordGeospatialCenter/MSA/master/Data/Stanford_MSA_Database.csv") %>% 
+  mutate(count = 1) %>% 
+  rename(year = Date, 
+         city = City, 
+         state = State) %>% 
+  select(year, city, state, count) %>% 
+  mutate(year = str_pad(year, width = 10, side = "left", "0"), 
+         year = substring(year , 7, 10), 
+         year = as_factor(year))
 
 # getting US geo data
 
 states <- map_data("state")
 
+# joing US and shooting data
+
+
+
+# blank US map
+
 s_map <- states %>% 
   ggplot() + 
   geom_polygon(aes(x = long, y = lat, group = group), color = "white") + 
   theme_void()
+
