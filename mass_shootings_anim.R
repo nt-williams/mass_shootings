c433050cbe1dba69c8a50753be89687c6298d293
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-11-12 21:00:56 -0500

    1971 being funky

--- mass_shootings_anim.R
+++ mass_shootings_anim.R
@@ -128,6 +128,52 @@ shooting_map <- function(yr) {
           plot.subtitle = element_text(size = 8, hjust = 0.006, color = "#4e4d47", 
                                        margin = margin(b = -0.1, t = 0.25, l = 10, unit = "cm"))) -> mp
   
+  file_out <- sprintf("mp-ms-%s.png", yr)
+  ggsave(file_out, mp, width = 8, height = 5)
+  
+  file_out
+}
+
+year <- levels(mass_shooting$year)
+
+year %>% 
+  purrr::map(shooting_map) %>% 
+  purrr::map(image_read) %>% 
+  image_join() %>% 
+  image_animate(fps = 2) %>% 
+  image_write("shooting.gif")
+  
+# gif with new years
+
+shooting_map <- function(yr) {
+  
+  mass_location %>% 
+    filter(year == yr) %>% 
+    ggplot() + 
+    geom_polygon(aes(x = long, y = lat, fill = n, group = group), color = NA, alpha = 0.8) + 
+    labs(title = "Number of victims due to mass shootings in the 48 continguous states", 
+         caption = "Source: Stanford Mass Shootings in America, courtesy of the Stanford Geospatial Center and Stanford Libraries", 
+         subtitle = paste("Year:", yr, sep = " ")) +
+    scale_fill_viridis(name = "Number of victims", 
+                       guide = guide_colorbar(barheight = unit(2, units = "mm"),
+                                              barwidth = unit(30, units = "mm"),
+                                              title.position = 'top', nrow = 1, 
+                                              ticks = FALSE),
+                       limits = c(0, 160)) + 
+    theme_void() + 
+    coord_map() + 
+    theme(plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+          legend.position = c(0.15, 0.09), 
+          legend.direction = "horizontal", 
+          plot.caption = element_text(size = 6, color = "#4e4d47",
+                                      margin = margin(b = 0.3, unit = "cm"), hjust = 0.98), 
+          legend.title = element_text(size = 8, color = "#4e4d47"), 
+          legend.text = element_text(size = 7, color = "#4e4d47"), 
+          plot.title = element_text(size = 10, hjust = 0.01, color = "#4e4d47", 
+                                    margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")), 
+          plot.subtitle = element_text(size = 8, hjust = 0.006, color = "#4e4d47", 
+                                       margin = margin(b = -0.1, t = 0.25, l = 10, unit = "cm"))) -> mp
+  
   file_out <- sprintf("mp-ms2-%s.png", yr)
   ggsave(file_out, mp, width = 8, height = 5)
   
@@ -142,7 +188,6 @@ year %>%
   image_join() %>% 
   image_animate(fps = 2) %>% 
   image_write("shooting2.gif")
-  
   
 
 
